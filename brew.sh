29997d4239dac7fbb14fc89c55c378bfd7f7cb85
Pawee Tantivasdakarn <mewpawee@Pawees-Mac-mini.local>
2022-11-17 00:33:53 +0700

    update

--- brew.sh
+++ brew.sh
@@ -1,3 +1,8 @@
-brew install nvim fzf exa asdf
+brew install nvim fzf asdf
 brew tap homebrew/cask-fonts
 brew install font-fira-code-nerd-font
+
+# rust utility
+# bat = cat
+# exa = ls
+brew install bat exa
