e3ac77f187a629619cb29142aa088b80e8c4c16c
Pawee Tantivasdakarn <mewpawee@Pawees-Mac-mini.local>
2022-11-17 00:19:32 +0700

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -147,7 +147,7 @@ formatters.setup {
   -- },
   {
     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
+    command = "prettierd",
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "typescript", "typescriptreact", "solidity" },
   },
