3a721bcdae019cb4a6958ed7de7501b1eae02f81
Pawee Tantivasdakarn <mewpawee@Pawees-Mac-mini.local>
2023-01-24 13:58:00 +0700

    feat: fixed vim colorscheme with thmux

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,8 +1,8 @@
 local options = {
     showtabline = 2, -- always show tab names on the top
     expandtab = true, -- convert tabs to spaces
-    shiftwidth = 4, -- the number of spaces inserted for each indentation
-    tabstop = 4, -- insert 2 spaces for a tab
+    shiftwidth = 2, -- the number of spaces inserted for each indentation
+    tabstop = 2, -- insert 2 spaces for a tab
     cursorline = true, -- highlight the current line
     number = true, -- set numbered lines
     relativenumber = true, --set relatives numbered lines
