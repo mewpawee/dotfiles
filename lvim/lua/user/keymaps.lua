251a5031db2f36fb251a48a24938d4481f01ba29
mewpawee <pawee.t@nilla.io>
2023-05-22 15:37:40 +0700

    feat: update keybinding

--- lvim/lua/user/keymaps.lua
+++ lvim/lua/user/keymaps.lua
@@ -1,13 +1,10 @@
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
+lvim.keys.normal_mode["ge"] = "G"                        -- jump to line end
+lvim.keys.normal_mode["gn"] = ":BufferLineCycleNext<CR>" -- next tab
+lvim.keys.normal_mode["gp"] = ":BufferLineCyclePrev<CR>" -- previouse tab
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>" -- save current tab
-lvim.keys.normal_mode["<C-d>"] = ":bd<cr>" -- close current tab(buffer)
-lvim.keys.normal_mode["|"] = ":vsplit<cr>" -- vertical split
-lvim.keys.normal_mode["_"] = ":split<cr>" -- horizontal split
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>" -- next tab
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>" -- previouse tab
--- unmap a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- override a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<C-w>q"] = ":bd<cr>"              -- close current tab(buffer)
+lvim.keys.normal_mode["<C-w>v"] = ":vs<cr>"              -- vertical split
+lvim.keys.normal_mode["<C-w>s"] = ":sp<cr>"              -- horizontal split
--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,13 +1,13 @@
 local options = {
-    showtabline = 2, -- always show tab names on the top
-    expandtab = true, -- convert tabs to spaces
-    shiftwidth = 2, -- the number of spaces inserted for each indentation
-    tabstop = 2, -- insert 2 spaces for a tab
-    cursorline = true, -- highlight the current line
-    number = true, -- set numbered lines
-    relativenumber = true, --set relatives numbered lines
+  showtabline = 2,         -- always show tab names on the top
+  expandtab = true,        -- convert tabs to spaces
+  shiftwidth = 2,          -- the number of spaces inserted for each indentation
+  tabstop = 2,             -- insert 2 spaces for a tab
+  cursorline = true,       -- highlight the current line
+  number = true,           -- set numbered lines
+  relativenumber = true,   --set relatives numbered lines
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
