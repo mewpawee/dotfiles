9d920c6667d81de5114cbb5b545a05bc5bac26cb
mewpawee <pawee.t@nilla.io>
2023-05-23 20:33:46 +0700

    feat: config mapping

--- lvim/lua/user/keymaps.lua
+++ lvim/lua/user/keymaps.lua
@@ -5,6 +5,11 @@ lvim.keys.normal_mode["ge"] = "G"                        -- jump to line end
 lvim.keys.normal_mode["gn"] = ":BufferLineCycleNext<CR>" -- next tab
 lvim.keys.normal_mode["gp"] = ":BufferLineCyclePrev<CR>" -- previouse tab
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>" -- save current tab
-lvim.keys.normal_mode["<C-w>q"] = ":bd<cr>"              -- close current tab(buffer)
-lvim.keys.normal_mode["<C-w>v"] = ":vs<cr>"              -- vertical split
-lvim.keys.normal_mode["<C-w>s"] = ":sp<cr>"              -- horizontal split
+
+-- File
+lvim.keys.normal_mode["U"] = "<C-r>" -- redo
+
+-- Buffer
+lvim.keys.normal_mode["<C-w>q"] = ":bd<cr>" -- close current tab(buffer)
+lvim.keys.normal_mode["<C-w>v"] = ":vs<cr>" -- vertical split
+lvim.keys.normal_mode["<C-w>s"] = ":sp<cr>" -- horizontal split
