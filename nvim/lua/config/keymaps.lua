2e4029eeeff90ba6298984ebc1b394ff68c662a2
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2024-01-22 17:24:55 +0700

    add move line for nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,3 +15,9 @@ keymap.set("n", "<C-s>", ":w<Return>")
 
 -- New tab
 keymap.set("n", "te", ":tabedit<Return>")
+
+-- Move Line
+--- move line up
+keymap.set("n", "<C-k>", "ddkP")
+--- move line down
+keymap.set("n", "<C-j>", "ddp")
