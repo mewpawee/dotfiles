d4bad5d9661f26cf852eb0551ff6cf6a624cfdd3
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2024-06-07 19:10:47 +0700

    update configs

--- scripts/foundry/anvil.sh
+++ scripts/foundry/anvil.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-anvil $(echo $(cat ~/.config/scripts/foundry/rpc.txt | gum choose --placeholder "Choose fork chain" \
-  | cut -d ':' -f 2,3))
+#!/bin/zsh
+
+# Chain to RPC endpoints list
+export LIST=$(cat <<END
+Mantle=https://rpc.mantle.xyz
+Blast=https://rpc.blast.io
+END
+)
+
+# filtering
+CHAIN=$(echo "$LIST" | cut -d'=' -f1 | gum filter)
+RPC=$(echo "$LIST" | grep $CHAIN | cut -d'=' -f2)
+
+# Run Anvil
+anvil -f $RPC
